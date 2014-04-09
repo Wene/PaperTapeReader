58213f0f4f32126fcf9e9c37bb7586f230218b4a
Wene <wene83@gmx.ch>
2014-04-09 13:29:22 +0200

    first working version

--- PaperTapeReader.ino
+++ PaperTapeReader.ino
@@ -1,17 +1,21 @@
+/* This software is written for Arduino boards and turn it into a
+   Control unit replacement for FACIT Paper tape reader. In the current
+   state only reading is supported. */
+
 void setup()
 {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // initialize the input pins
-  pinMode(2, INPUT_PULLUP); // bit 0
-  pinMode(3, INPUT_PULLUP); // bit 1
-  pinMode(4, INPUT_PULLUP); // bit 2
-  pinMode(5, INPUT_PULLUP); // bit 3
-  pinMode(6, INPUT_PULLUP); // bit 4
-  pinMode(7, INPUT_PULLUP); // bit 5
-  pinMode(8, INPUT_PULLUP); // bit 6
-  pinMode(9, INPUT_PULLUP); // bit 7 or parity
-  pinMode(10, INPUT_PULLUP); // clock
+  pinMode(2, INPUT); // bit 0
+  pinMode(3, INPUT); // bit 1
+  pinMode(4, INPUT); // bit 2
+  pinMode(5, INPUT); // bit 3
+  pinMode(6, INPUT); // bit 4
+  pinMode(7, INPUT); // bit 5
+  pinMode(8, INPUT); // bit 6
+  pinMode(9, INPUT); // bit 7 or parity
+  pinMode(10, INPUT); // clock
 }
 
 void loop()
@@ -29,7 +33,7 @@ void loop()
   for(int i = 0; i < 8; i++)
   {
     bool state = digitalRead(i+2); 
-    bit[i] = !state;
+    bit[i] = state;
   }
   
   for(int i = 0; i < 8; i++)
@@ -38,6 +42,7 @@ void loop()
   }
   
   Serial.println();
+  delay(3);
   while(!wait)
   {
     if(digitalRead(10) == HIGH)
