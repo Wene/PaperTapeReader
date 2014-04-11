e1b4a365a5194e0891db9a46ddb0ac005721237e
Wene <wene83@gmx.ch>
2014-04-11 13:37:10 +0200

    refactoring - a bit more structure

--- PaperTapeReader.ino
+++ PaperTapeReader.ino
@@ -1,14 +1,17 @@
 /* This software is written for Arduino boards and turn it into a
    Control unit replacement for FACIT Paper tape reader. In the current
-   state only reading is supported. */
+   state three read modes are available: debug, binary and ASCII. */
 
-char mode = 'e';
+enum readMode {error, debug, binary, ascii};
+
+readMode mode = error;
 unsigned int errorCount = 0;
 
 void setup()
 {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(19200);
+  
   // initialize the input pins
   pinMode(2, INPUT); // bit 0
   pinMode(3, INPUT); // bit 1
@@ -50,42 +53,41 @@ void loop()
   while(Serial.available())
   {
     int command;
+    unsigned char letter;
     
     command = Serial.read();
-    mode = command;
+    letter = command;
     
     Serial.println();
     
-    if(mode == 'd' || mode == 'D')
+    switch(letter)
     {
-      //debug mode selected
-      mode = 'd';
-      Serial.println("Enter debug mode. Press a key to exit.");
-    }
-    else if(mode == 'b' || mode == 'B')
-    {
-      //binary mode selscted
-      mode = 'b';
-      Serial.println("Enter binary mode. Press a key to exit.");
-    }
-    else if(mode == 'a' || mode == 'A')
-    {
-      //ASCII mode selected
-      mode = 'a';
-      Serial.println("Enter ASCII mode. Press a key to exit.");
-    }
-    else
-    {
-      //selection error
-      mode = 'e';
-      Serial.println("Error - could not interpret your selection. Try again.");
+      case 'd':
+      case 'D':
+        mode = debug;
+        Serial.println("Enter debug mode. Press a key to exit.");
+        break;
+      case 'b':
+      case 'B':    
+        mode = binary;
+        Serial.println("Enter binary mode. Press a key to exit.");
+        break;
+      case 'a':
+      case 'A':
+        mode = ascii;
+        Serial.println("Enter ASCII mode. Press a key to exit.");
+        break;
+      default:
+        mode = error;
+        Serial.println("Error - could not interpret your selection. Try again.");
+        break;
     }
     
-    Serial.println();
+    Serial.println();   
   }
   
   //read loop - read until a key is pressed
-  while(!Serial.available() && mode != 'e')
+  while(!Serial.available() && mode != error)
   {
     bool clockSignal = false;
     if(digitalRead(10) == HIGH)
@@ -102,66 +104,57 @@ void loop()
         bit[i] = state;
       }
       
-      //debug mode
-      if(mode == 'd')
+      //combine bits to a bytes
+      unsigned char byte8;
+      for(int i = 0; i < 8; i++)
       {
-	//print binary code
-        for(int i = 0; i < 8; i++)
-        {
-          Serial.print(bit[i]);
-        }
-        Serial.print("\t");
+        bitWrite(byte8, i, bit[i]);
+      }
+      unsigned char byte7;
+      for(int i = 0; i < 7; i++)
+      {
+        bitWrite(byte7, i, bit[i]);
+      }
+      
+      switch(mode)
+      {
+        case debug:
+          //print binary code
+          for(int i = 0; i < 8; i++)
+          {
+            Serial.print(bit[i]);
+          }
+          
+          Serial.print("\t");
         
-	//combine byte and print it in Hex and Dec
-	unsigned char byte;
-	for(int i = 0; i < 8; i++)
-	{
-	  bitWrite(byte, i, bit[i]);
-	}
-	Serial.print(byte, HEX);
-	Serial.print("\t");
-	Serial.print(byte, DEC);
-        Serial.println();
+          //print in Hex and Dec
+          Serial.print(byte8, HEX);
+          Serial.print("\t");
+          Serial.print(byte8, DEC);
+          Serial.println();
+          break;
+        case ascii:
+          //ignore zeroes
+          if(byte7)
+          {
+
+            //check parity and count errors
+            int bitCount = 0;
+            for(int i = 0; i < 8; i++)
+            {
+              bitCount += bit[i];
+            }
+            errorCount += bitCount % 2;
+
+            //print the character
+            Serial.write(byte7);
+          }
+          break;
+        case binary:
+          Serial.write(byte8);
+          break;
       }
       
-      //ASCII mode
-      else if(mode == 'a')
-      {
-	//combine ASCII character
-	char byte;
-	for(int i = 0; i < 7; i++)
-	{
-	  bitWrite(byte, i, bit[i]);
-	}
-	
-	//ignore zeroes
-	if(byte)
-	{
-	
-	  //check parity and count errors
-	  int bitCount = 0;
-	  for(int i = 0; i < 8; i++)
-	  {
-	    bitCount += bit[i];
-	  }
-	  errorCount += bitCount % 2;
-	
-	  //print the character
-	  Serial.write(byte);
-	}
-      }
-      
-      //binary mode
-      else if(mode == 'b')
-      {
-	unsigned char byte;
-	for(int i = 0; i < 8; i++)
-	{
-	  bitWrite(byte, i, bit[i]);
-	}
-	Serial.write(byte);
-      }
-  
       delay(3);
     }
   }
