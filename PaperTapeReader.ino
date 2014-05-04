76b622af9770ae76c54b569f476c7d81d9a9a5a7
Wene <wene83@gmx.ch>
2014-05-04 17:37:07 +0200

    5-Bit Baudot mode added

--- PaperTapeReader.ino
+++ PaperTapeReader.ino
@@ -1,11 +1,99 @@
 /* This software is written for Arduino boards and turn it into a
    Control unit replacement for FACIT Paper tape reader. In the current
-   state three read modes are available: debug, binary and ASCII. */
+   state four read modes are available: debug, binary, ASCII and Baudot. */
 
-enum readMode {error, debug, binary, ascii};
+enum readMode {error, debug, binary, ascii, baudot};
+enum baudotMode {letters, figures};
 
 readMode mode = error;
 unsigned int errorCount = 0;
+baudotMode bMode = letters;
+
+unsigned char baudot2ascii(unsigned char inchar)
+{
+  //remove the last 3 bits, which are allways 1 by reading 5 bit code
+  inchar -= 224;
+  
+  unsigned char outchar = 0;
+  
+  if(bMode == letters)
+  {
+    switch(inchar)
+    {
+      case 1:   outchar = 't';   break;
+      case 2:   outchar = 13;    break; //CR
+      case 3:   outchar = 'o';   break;
+      case 4:   outchar = ' ';   break; //space
+      case 5:   outchar = 'h';   break;
+      case 6:   outchar = 'n';   break;
+      case 7:   outchar = 'm';   break;
+      case 8:   outchar = 10;    break; //LF
+      case 9:   outchar = 'l';   break;
+      case 10:  outchar = 'r';   break;
+      case 11:  outchar = 'g';   break;
+      case 12:  outchar = 'i';   break;
+      case 13:  outchar = 'p';   break;
+      case 14:  outchar = 'c';   break;
+      case 15:  outchar = 'v';   break;
+      case 16:  outchar = 'e';   break;
+      case 17:  outchar = 'z';   break;
+      case 18:  outchar = 'd';   break;
+      case 19:  outchar = 'b';   break;
+      case 20:  outchar = 's';   break;
+      case 21:  outchar = 'y';   break;
+      case 22:  outchar = 'f';   break;
+      case 23:  outchar = 'x';   break;
+      case 24:  outchar = 'a';   break;
+      case 25:  outchar = 'w';   break;
+      case 26:  outchar = 'j';   break;
+      case 27:  bMode = figures; break;
+      case 28:  outchar = 'u';   break;
+      case 29:  outchar = 'q';   break;
+      case 30:  outchar = 'k';   break;
+      //31 is shift to letters. Ignored because still in this mode.
+      default:  outchar = 0;     break;
+    }
+  }
+  else if(bMode == figures)
+  {
+    switch(inchar)
+    {
+      case 1:   outchar = '5';   break;
+      case 2:   outchar = 13;    break; //CR
+      case 3:   outchar = '9';   break;
+      case 4:   outchar = ' ';   break;
+      case 5:   outchar = ' ';   break; //unused od #
+      case 6:   outchar = ',';   break;
+      case 7:   outchar = '.';   break;
+      case 8:   outchar = 10;    break; //LF
+      case 9:   outchar = ')';   break;
+      case 10:  outchar = '4';   break;
+      case 11:  outchar = ' ';   break; //unused or &
+      case 12:  outchar = '8';   break;
+      case 13:  outchar = '0';   break;
+      case 14:  outchar = ':';   break;
+      case 15:  outchar = '=';   break;
+      case 16:  outchar = '3';   break;
+      case 17:  outchar = '+';   break;
+      case 18:  outchar = 5;     break; //Who are You? -> ASCII representation ENQuiry
+      case 19:  outchar = '?';   break;
+      case 20:  outchar = '\'';  break;
+      case 21:  outchar = '6';   break;
+      case 22:  outchar = ' ';   break; //unused or !
+      case 23:  outchar = '/';   break;
+      case 24:  outchar = '-';   break;
+      case 25:  outchar = '2';   break;
+      case 26:  outchar = '\a''; break; //Bell
+      //27 is shift to figures. Ignored because still in this mode.
+      case 28:  outchar = '7';   break;
+      case 29:  outchar = '1';   break;
+      case 30:  outchar = '(';   break;
+      case 31:  bMode = letters; break;
+      default:  outchar = 0;     break;
+    }
+  }  
+  return outchar;
+}
 
 void setup()
 {
@@ -45,7 +133,7 @@ void loop()
   }
   
   //print prompt
-  Serial.print("Select mode: (D)ebug, (B)inary, (A)SCII");
+  Serial.print("Select mode: (D)ebug, (B)inary, (A)SCII, (5)-Bit Baudot");
   while(!Serial.available())
   {
     delay(10);
@@ -79,6 +167,10 @@ void loop()
       case 'A':
         mode = ascii;
         Serial.println("Enter ASCII mode. Press a key to exit.");
+        break;
+      case '5':
+        mode = baudot;
+        Serial.println("Enter 5-Bit Baudot mode. Press a key to exit.");
         break;
       default:
         mode = error;
@@ -125,7 +217,18 @@ void loop()
           //print binary code
           for(int i = 0; i < 8; i++)
           {
-            Serial.print(bit[i]);
+            if(bit[i])
+            {
+              Serial.print("o");
+            }
+            else
+            {
+              Serial.print(".");
+            }
+            if(i == 2)
+            {
+              Serial.print(":");
+            }
           }
           
           Serial.print("\t");
@@ -164,6 +267,15 @@ void loop()
           
         case binary:
           Serial.write(byte8);
+          break;
+          
+        case baudot:
+          //substitute baudot code by ASCII character and print it
+          unsigned char b2ascii = baudot2ascii(byte8);
+          if(b2ascii) //only write characters != 0
+          {
+            Serial.write(b2ascii);
+          }
           break;
       }
       
